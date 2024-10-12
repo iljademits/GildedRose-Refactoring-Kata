0180f9b803a05c8993c6a6d5988f0e5f52d9237c
iljademits <iljademits@localhost>
2024-10-12 14:00:25 +0200

    Step 4: Separate out conditions second if-else block updateQuality()

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -41,20 +41,16 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                if (items[i].name.equals("Aged Brie")) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
+                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                	items[i].quality = items[i].quality - items[i].quality;
+                } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                	// DO NOTHING
+                } else {
+                	items[i].quality = items[i].quality - 1;
                 }
             }
         }
