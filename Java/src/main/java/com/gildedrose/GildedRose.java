55b40afcbcce078e011defe291a3cf094483eb32
iljademits <iljademits@localhost>
2024-10-12 13:51:44 +0200

    Step 3: Move decrease sellIn as first step

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+        	
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
             
             if (items[i].name.equals("Aged Brie")) {
                 if (items[i].quality < 50) {
@@ -18,13 +22,13 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].sellIn < 11) {
+                    if (items[i].sellIn < 10) {
                         if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
 
-                    if (items[i].sellIn < 6) {
+                    if (items[i].sellIn < 5) {
                         if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
@@ -33,11 +37,7 @@ class GildedRose {
             } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
             	// DO NOTHING
             } else if (items[i].quality > 0) {
-            		items[i].quality = items[i].quality - 1;
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+        		items[i].quality = items[i].quality - 1;
             }
 
             if (items[i].sellIn < 0) {
